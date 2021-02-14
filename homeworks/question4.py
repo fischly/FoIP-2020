69c741e5a6ddb3e572ca91fd64d13e99241719d4
Felix Mitterer <felix.mitterer@posteo.de>
2021-02-14 20:31:12 +0100

    fixed question 4

--- homeworks/question4.py
+++ homeworks/question4.py
@@ -15,13 +15,17 @@ for i in range(10):
     im_d = cv2.dilate(im,W,iterations = 1)
     im_e = cv2.erode(im,W,iterations = 1)
     im_h = cv2.addWeighted(im_d,0.5,im_e,0.5,0)
-    if (im > im_h).all():
-        im = im_d
-    else:
-        im = im_e
+    
+    # perform this test for each pixel
+    for y in range(im.shape[1]):
+        for x in range(im.shape[0]):
+            im[x, y] = im_d[x, y] if im[x, y] >= im_h[x, y] else im_e[x, y]
         
 # plot results
 fig = plt.figure(figsize=(19.20,10.80))
 plt.subplot(121),plt.imshow(img,cmap='gray'), plt.title('Original')
 plt.subplot(122),plt.imshow(im,cmap='gray'), plt.title('Edited')
 plt.show()
+
+# save resulting image
+plt.imsave('./results/question_4_road_sign_school_blurry_filtered.jpg', im, cmap = 'gray')
