085dba221bd076e8726f77432a70eb95a39febe9
Evi Fasouli <evi.f.94+github@gmail.com>
2021-02-14 15:54:02 +0100

    Update question1.py

--- homeworks/question1.py
+++ homeworks/question1.py
@@ -12,16 +12,17 @@ img3= cv2.imread('./imagesHW/hw1_painting_2_reference.JPG')
 img4= cv2.imread('./imagesHW/hw1_painting_2_tampered.JPG')
 
 # subtract images & convert to grayscale
-img12 = cv2.subtract(img1,img2)
+img12 = cv2.absdiff(img1,img2)
 img12 =cv2.cvtColor(img12, cv2.COLOR_BGR2GRAY)
-# convert 0 pixels to black and everything else to white
-img12b = cv2.threshold(img12, 0, 255, cv2.THRESH_BINARY)[1]
+# convert pixels with difference >10 to white and everything else to black
+img12b = cv2.threshold(img12, 10, 255, cv2.THRESH_BINARY)[1]
 
-img34 = cv2.subtract(img3,img4)
+img34 = cv2.absdiff(img3,img4)
 img34 =cv2.cvtColor(img34, cv2.COLOR_BGR2GRAY)
-img34b = cv2.threshold(img34, 0, 255, cv2.THRESH_BINARY)[1]
+img34b = cv2.threshold(img34, 10, 255, cv2.THRESH_BINARY)[1]
 
 # plot results
+fig = plt.figure(figsize=(19.20,10.80))
 plt.subplot(121),plt.imshow(img12b,cmap='gray'), plt.title('Painting1')
 plt.subplot(122),plt.imshow(img34b,cmap='gray'), plt.title('Painting2')
 plt.show()
