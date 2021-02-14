fbc3fe1911cf08ea22489490a85f1fa1464f3585
Evi Fasouli <evi.f.94+github@gmail.com>
2021-02-14 20:33:30 +0100

    Update question4.py

--- homeworks/question4.py
+++ homeworks/question4.py
@@ -2,25 +2,32 @@
 """
 Homeworks - Question 4
 """
-import cv2
-from matplotlib import pyplot as plt
-
-# read image
+# read the image
 img= cv2.imread('./imagesHW/hw3_road_sign_school_blurry.JPG',0)
-im = img.copy()
-W = cv2.getStructuringElement(cv2.MORPH_CROSS,(3,3)) # cross kernel
 
-# apply the algorithm
+# copy the image to perform the transformations
+im = img.copy()
+
+# figure out how large the image is
+height, width = im.shape[:2]
+
+# select the kernel that is used for dilation-erosion
+W = cv2.getStructuringElement(cv2.MORPH_CROSS,(5,5))
+
+# apply the given algorithm
 for i in range(10):
-    im_d = cv2.dilate(im,W,iterations = 1)
-    im_e = cv2.erode(im,W,iterations = 1)
-    im_h = cv2.addWeighted(im_d,0.5,im_e,0.5,0)
+    im_d = cv2.dilate(im,W) # first dilate
+    im_e = cv2.erode(im,W)  # then erode
+    im_h = cv2.addWeighted(im_d,0.5,im_e,0.5,0) # then add images
+    # for every pixel perform the following test
+    for x in range(0,height):
+        for y in range(0,width):
+            pixel = im[x,y] # current pixel
+            if pixel > im_h[x,y]:
+                pixel = im_d[x,y]
+            else:
+                pixel = im_e[x,y]
     
-    # perform this test for each pixel
-    for y in range(im.shape[1]):
-        for x in range(im.shape[0]):
-            im[x, y] = im_d[x, y] if im[x, y] >= im_h[x, y] else im_e[x, y]
-        
 # plot results
 fig = plt.figure(figsize=(19.20,10.80))
 plt.subplot(121),plt.imshow(img,cmap='gray'), plt.title('Original')
